c58c2c7a3c94d6150f322bae274513f95f3ff7d4
hamidtechdestination-cmd <hamidtechdestination@gmail.com>
2026-01-02 16:42:47 -0800

    responce

--- backend/src/utils/apierrors.js
+++ backend/src/utils/apierrors.js
@@ -1,25 +1,51 @@
-class apierror extends error {
+// class apierror extends error {
+//     constructor(
+//         statuscode,
+//         message = "something went wrong",
+//         errors = [],
+//         stack = ""
+//     ) {
+//         super(message)
+//         this.statuscode = statuscode
+//         this.data = null
+//         this.message = message
+//         this.success = false;
+//         this.errors = this.errors
+
+
+//         if (stack) {
+//             this.stack = stack
+//         }
+//         else {
+//             Error.captureStackTrace(this.globalThis.costructore)
+//         }
+//     }
+// }
+
+// export { apierror }
+
+
+class ApiError extends Error {
     constructor(
-        statuscode,
-        message = "something went wrong",
+        statusCode,
+        message = "Something went wrong",
         errors = [],
         stack = ""
     ) {
-        super(message)
-        this.statuscode = statuscode
-        this.data = null
-        this.message = message
+        super(message);
+        this.statusCode = statusCode;
+        this.data = null; // Error mein data hamesha null hota hai
+        this.message = message;
         this.success = false;
-        this.errors = this.errors
-
+        this.errors = errors; // Fixed: using the parameter
 
         if (stack) {
-            this.stack = stack
-        }
-        else {
-            Error.captureStackTrace(this.globalThis.costructore)
+            this.stack = stack;
+        } else {
+            // Fixed: correct way to capture stack trace
+            Error.captureStackTrace(this, this.constructor);
         }
     }
 }
 
-export { apierror }
+export { ApiError };
